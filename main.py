87d8b8722b7437e69e80f74fe9aaace1ff932449
NaimRahmanIfti <naimrahmanifti@gmail.com>
2025-07-27 07:40:59 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        return "Post request received"
+    return render_template("edit.html")
+
+app.run(debug=True, port=5001)
